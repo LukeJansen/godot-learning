5dc63893f3879893a2d962f8fa4a687716783d21
LukeJansen <lpjansen@live.co.uk>
2023-09-20 17:36:52 +0100

    Initial Test Level & Basic Player Movement

---
+++ scripts/player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+
+@export var SPEED = 300.0
+@export var JUMP_HEIGHT = 400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = -JUMP_HEIGHT
+
+	# Get the input direction and handle the movement/deceleration.
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
